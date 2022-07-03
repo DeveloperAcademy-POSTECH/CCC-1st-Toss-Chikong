ad974735fbab66b248205028b751e2f78ea4df7b
jeohong <jeohong59@gmail.com>
2022-07-03 11:35:38 +0900

    project Init

---
+++ Toss/Toss/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Toss
+//
+//  Created by Hong jeongmin on 2022/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

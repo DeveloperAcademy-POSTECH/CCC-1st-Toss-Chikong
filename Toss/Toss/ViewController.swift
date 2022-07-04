fa8bc8004079d8edc4f5f67a9e9d8758be5ea59d
jeohong <jeohong59@gmail.com>
2022-07-04 19:50:07 +0900

    Feat: without Storyboard Setting

--- Toss/Toss/ViewController.swift
+++ Toss/Toss/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.title = "hello"
+        self.view.backgroundColor = .white
     }
-
-
 }
-
